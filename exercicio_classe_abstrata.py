7db31446285455600e19a603e16be447a06ceb5d
laryssastephanie <laryssacosta-@hotmail.com>
2021-05-11 15:20:34 -0300

    Exceçoes personalizadas adicionadas

--- exercicio_classe_abstrata.py
+++ exercicio_classe_abstrata.py
@@ -1,31 +1,28 @@
 import math
+from abc import ABC, abstractmethod
 
-class FormaGeometrica:
+class FormaGeometrica(ABC):
     def __init__(self, x, y):
         self.x, self.y = x, y
 
-    def desenhe(self):
-        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y})" + "\n")
+    @abstractmethod
+    def calculaArea(self):
+        pass
 
-f1 = FormaGeometrica(0, 0)
-f1.desenhe()
+    @abstractmethod
+    def desenhe(self):
+        pass
+
 
 class Forma2D(FormaGeometrica):
     def __init__(self, x, y):
         super().__init__(x, y)
 
 
-f1_2D = Forma2D(0, 0)
-f1_2D.desenhe()
-
 class Forma3D(FormaGeometrica):
     def __init__(self, x, y, z):
         super().__init__(x, y)
         self.z = z
-        
-
-f1_3D = Forma3D(0, 0, 0)
-f1_3D.desenhe()
 
 
 class Poligono(Forma2D):
@@ -35,12 +32,14 @@ class Poligono(Forma2D):
         self.tamanho_lados = tamanho_lados
         self.apotema = apotema
 
-    def desenhe(self):
+    def calculaArea(self):
         perimetro = self.quantidade_lados * self.tamanho_lados
         area = (perimetro * self.apotema) / 2
-        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y})")
         print("O perímetro do Polígono é " + str(perimetro))
-        print("A área do Polígono é " + str(area) + "\n")
+        print("A área do Polígono é " + str(area))
+
+    def desenhe(self):
+        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y}) \n")
 
     def compara_poligono(self, outro_poligono):
         return self.x == outro_poligono.x and self.y == outro_poligono.y and self.quantidade_lados == outro_poligono.quantidade_lados and self.tamanho_lados == outro_poligono.tamanho_lados and self.apotema == outro_poligono.apotema
@@ -64,6 +63,7 @@ class Poligono(Forma2D):
         return outro.compara_poligono(self)
 
 pol_2D = Poligono(0, 0, 5, 2, 1)
+pol_2D.calculaArea()
 pol_2D.desenhe()
 
 class Retangulo(Forma2D):
@@ -72,12 +72,14 @@ class Retangulo(Forma2D):
         self.altura = altura
         self.largura = largura
 
-    def desenhe(self):
+    def calculaArea(self):
         area = self.altura * self.largura
         perimetro = 2 * (self.altura + self.largura)
-        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y})")
         print(f"O perímetro do {type(self).__name__} é " + str(perimetro))
-        print(f"A área do {type(self).__name__} é " + str(area) + "\n")
+        print(f"A área do {type(self).__name__} é " + str(area))
+
+    def desenhe(self):
+        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y}) \n")
 
     def compara_retangulo(self, outro_retangulo):
         return self.x == outro_retangulo.x and self.y == outro_retangulo.y and self.altura == outro_retangulo.altura and self.largura == outro_retangulo.largura
@@ -102,6 +104,7 @@ class Retangulo(Forma2D):
 
 
 ret_2D = Retangulo(0, 0, 2, 5)
+ret_2D.calculaArea()
 ret_2D.desenhe()
 
 class Quadrado(Retangulo):
@@ -131,21 +134,25 @@ class Quadrado(Retangulo):
     
 
 qd_2D = Quadrado(0, 0, 4, 4)
+qd_2D.calculaArea()
 qd_2D.desenhe()
+
 
 class Esfera(Forma3D):
     def __init__(self, x, y, z, raio):
         super().__init__(x, y, z)
         self.raio = raio
 
-    def desenhe(self):
+    def calculaArea(self):
         area = 4 * math.pi * (self.raio ** 2)
         perimetro = 2 * math.pi * self.raio
         volume = (4 * math.pi * (self.raio ** 3)) / 3
-        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y})")
         print(f"O perímetro do {type(self).__name__} é " + str(perimetro))
         print(f"A área do {type(self).__name__} é " + str(area))
-        print(f"O volume do {type(self).__name__} é " + str(volume) + "\n")
+        print(f"O volume do {type(self).__name__} é " + str(volume))
+
+    def desenhe(self):
+        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y}) \n")
 
     def compara_esfera(self, outro_esfera):
         return self.x == outro_esfera.x and self.y == outro_esfera.y and self.z == outro_esfera.z and self.raio == outro_esfera.raio
@@ -169,6 +176,7 @@ class Esfera(Forma3D):
         return outro.compara_esfera(self)
 
 esf = Esfera(0, 0, 0, 5)
+esf.calculaArea()
 esf.desenhe()
 
 class Cubo(Forma3D):
@@ -176,14 +184,16 @@ class Cubo(Forma3D):
         super().__init__(x, y, z)
         self.aresta = aresta
 
-    def desenhe(self):
+    def calculaArea(self):
         area = 6 * self.aresta ** 2
         perimetro = 12 * self.aresta
         volume = self.aresta ** 3
-        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y})")
         print(f"O perímetro do {type(self).__name__} é " + str(perimetro))
         print(f"A área do {type(self).__name__} é " + str(area))
-        print(f"O volume do {type(self).__name__} é " + str(volume) + "\n")
+        print(f"O volume do {type(self).__name__} é " + str(volume))
+
+    def desenhe(self):
+        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y}) \n")
 
     def compara_cubo(self, outro_cubo):
         return self.x == outro_cubo.x and self.y == outro_cubo.y and self.z == outro_cubo.z and self.aresta == outro_cubo.aresta
@@ -207,6 +217,7 @@ class Cubo(Forma3D):
         return outro.compara_cubo(self)
 
 cb = Cubo(0, 0, 0, 2)
+cb.calculaArea()
 cb.desenhe()
 
 class Cilindro(Forma3D):
@@ -215,14 +226,16 @@ class Cilindro(Forma3D):
         self.raio = raio
         self.altura = altura
 
-    def desenhe(self):
+    def calculaArea(self):
         areaBase = math.pi * (self.raio ** 2)
         areaLateral = 2 * math.pi * self.altura * self.raio
         areaTotal = (2 * areaBase) + areaLateral
         volume = areaBase * self.altura
-        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y})")
         print(f"A área do {type(self).__name__} é " + str(areaTotal))
-        print(f"O volume do {type(self).__name__} é " + str(volume) + "\n")
+        print(f"O volume do {type(self).__name__} é " + str(volume))
+
+    def desenhe(self):
+        print(f"{type(self).__name__} centrada(o) em ({self.x}, {self.y}) \n")
 
     def compara_cilindro(self, outro_cilindro):
         return self.x == outro_cilindro.x and self.y == outro_cilindro.y and self.z == outro_cilindro.z and self.raio == outro_cilindro.raio and self.altura == outro_cilindro.altura
@@ -246,11 +259,18 @@ class Cilindro(Forma3D):
         return outro.compara_cilindro(self)
 
 cl = Cilindro(0, 0, 0, 3, 5)
+cl.calculaArea()
 cl.desenhe()
 
+class limiteMax(Exception):
+    pass
+
+def raiseLimiteMax():
+    raise limiteMax()
 
 class Quadro:
-    def __init__(self):
+    def __init__(self, qntd_max = 11):
+        self.qntd_max = qntd_max
         self.figuras_geometricas = []
 
     def incluir_na_lista(self, figura):
@@ -261,12 +281,23 @@ class Quadro:
                     return True
             return False
         if (not contem(self, figura)):
-            self.figuras_geometricas.append(figura)
+            if(len(self.figuras_geometricas) >= self.qntd_max):
+                raise limiteMax(f"\nLimite atingido. Máximo: {self.qntd_max}\n") # a quantidade máxima atribuida foi de 11. Ao tentar adicionar o 12º elemento (len da lista será maior que a qntd_max), retornará a exceção
+            else:
+              self.figuras_geometricas.append(figura)
         else:
             print(f"\nFigura {figura} já existe na lista.\n")
 
-    def remover(self, idx):
+    def remover(self, idx): # remove pela posição da figura
+        if not isinstance(idx, int):
+            raise TypeError("Somente tipo inteiro") # só recebe tipo inteiro. Se digitar outro tipo, retornará exceção.
+        if (idx > len(self.figuras_geometricas)):
+            raise ValueError("Posição Inválida") # Se pedir para remover algum índice maior do que o tamanho da lista, retornará exceção.
         self.figuras_geometricas.pop(idx)
+
+    def calculaArea(self):
+        for figura in self.figuras_geometricas:
+            figura.calculaArea()
 
     def desenhe(self):
         for figura in self.figuras_geometricas:
@@ -277,12 +308,11 @@ class Quadro:
             print("==========", idx, "==========")
             print(self.figuras_geometricas[idx])
 
-    def remover_forma(self, figura):
+    def remover_forma(self, figura): # remove pela forma, não pelo índice
         for i, f in enumerate(self.figuras_geometricas):
             if (f == figura):
                 self.remover(i)
                 break
-
 
 ret1 = Retangulo(0, 0, 2, 4)
 ret2 = Retangulo(0, 0, 4, 8)
@@ -298,26 +328,35 @@ cb2 = Cubo(0, 0, 0, 10)
 cl1 = Cilindro(0, 0, 0, 5, 10)
 cl2 = Cilindro(0, 0, 0, 10, 20)
 
-qd = Quadro()
-
-qd.incluir_na_lista(ret1)
-qd.incluir_na_lista(ret2)
-qd.incluir_na_lista(ret4)
-qd.incluir_na_lista(q1)
-qd.incluir_na_lista(q2)
-qd.incluir_na_lista(pol1)
-qd.incluir_na_lista(pol2)
-qd.incluir_na_lista(es1)
-qd.incluir_na_lista(es2)
-qd.incluir_na_lista(cb1)
-qd.incluir_na_lista(cb2)
-qd.incluir_na_lista(cl1)
-qd.incluir_na_lista(cl2)
+qd = Quadro(11)
+try:
+    qd.incluir_na_lista(ret1)
+    qd.incluir_na_lista(ret2)
+    qd.incluir_na_lista(ret4)
+    qd.incluir_na_lista(q1)
+    qd.incluir_na_lista(q2)
+    qd.incluir_na_lista(pol1)
+    qd.incluir_na_lista(pol2)
+    qd.incluir_na_lista(es1)
+    qd.incluir_na_lista(es2)
+    qd.incluir_na_lista(cb1)
+    qd.incluir_na_lista(cb2)
+    qd.incluir_na_lista(cl1)
+    qd.incluir_na_lista(cl2)
+except limiteMax as e:
+   print(f"\nOcorreu um erro: {e.__class__}\n")
+except NameError:
+    print(f"\n Forma inválida\n")
 
 qd.desenhe()
 qd.contador()
 
-qd.remover(2)
+try:
+    qd.remover(4.4)
+except ValueError:
+    print("\nOcorreu um erro de valor")
+except TypeError:
+    print("\nOcorreu um erro de tipo")
 
 print("\n********* Removendo figura *********\n")
 
@@ -328,5 +367,6 @@ print("\n********** Removendo Outra Figura **********\n")
 
 rt3 = Retangulo(0, 0, 2, 4) # mesmo rt3 não tendo sido adicionado na lista, ele é idêntico ao rt1, portanto rt1 será removido por comparação.
 qd.remover_forma(rt3)
+qd.calculaArea()
 qd.desenhe()
 qd.contador()
